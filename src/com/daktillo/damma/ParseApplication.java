620cd502fc636032c3cc0a1ac36a5c314e0c4f1c
Tufan Türkaslan <dofeyna@hotmail.com>
2013-11-28 14:02:43 +0200

    New Branch

--- src/com/daktillo/damma/ParseApplication.java
+++ src/com/daktillo/damma/ParseApplication.java
@@ -22,7 +22,7 @@ public class ParseApplication extends Application {
 	    
 		// If you would like all objects to be private by default, remove this line.
 		defaultACL.setPublicReadAccess(true);
-		
+				
 		ParseACL.setDefaultACL(defaultACL, true);
 	}
 
