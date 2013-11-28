fbddde9656ac45497806abcba705d18df61f4dd4
Seakar <android.developerr@gmail.com>
2013-11-28 13:16:15 +0200

    Deneme
    int a = 5;

--- src/com/daktillo/damma/ParseApplication.java
+++ src/com/daktillo/damma/ParseApplication.java
@@ -23,6 +23,8 @@ public class ParseApplication extends Application {
 		// If you would like all objects to be private by default, remove this line.
 		defaultACL.setPublicReadAccess(true);
 		
+		int a = 5;
+		
 		ParseACL.setDefaultACL(defaultACL, true);
 	}
 
